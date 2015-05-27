30edea5bc7ac1813fb45fe4c2c59aae6c88af9f2
Sebastian Hubenschmid <sebastian@ioncube.com>
2015-05-27 07:09:40 +0100

    Workstation changes

--- system/env.zsh
+++ system/env.zsh
@@ -1,4 +1,5 @@
 # Only set this if we haven't set $EDITOR up somewhere else previously.
 if [[ "$EDITOR" == "" ]] ; then
-  export EDITOR='vim'
+
+    if command -v nvim >/dev/null 2>&1; then export EDITOR='nvim'; else export EDITOR='vim'; fi
 fi
