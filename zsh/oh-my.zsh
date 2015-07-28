ca315731d0a9bc5a5baba947f2effa3d309df293
Sebastian Hubenschmid <sebastian@ioncube.com>
2015-07-28 15:50:10 +0100

    Reenabled tmux autostart

--- zsh/oh-my.zsh
+++ zsh/oh-my.zsh
@@ -16,7 +16,7 @@ source $HOME/.oh-my-zsh/oh-my-zsh.sh
 # Start tmux
 # If not running interactively, do not do anything
 #[[ $- != *i* ]] && return
-#[[ $TERM != screen* ]] && exec tmux -2
+[[ $TERM != screen* ]] && exec tmux -2
 
 # Set color
 export TERM=xterm-256color
