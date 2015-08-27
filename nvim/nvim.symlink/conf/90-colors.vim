673c76a1fd13b51b4733fd3bb8d9f63c3dee6d76
Sebastian Hubenschmid <sebastian@ioncube.com>
2015-08-27 16:17:39 +0100

    Use jellybeans colorscheme, base16 didnt work out

--- nvim/nvim.symlink/conf/90-colors.vim
+++ nvim/nvim.symlink/conf/90-colors.vim
@@ -1,8 +1,5 @@
 " Colorscheme
     set t_Co=256
     set background=dark
-    "colorscheme molokai
-    let base16colorspace=256
-    "colorscheme PaperColor
-    colorscheme base16-default
+    colorscheme jellybeans
 
--- zsh/colour.zsh
+++ zsh/colour.zsh
@@ -1,3 +1,3 @@
-BASE16_SHELL="$HOME/.config/base16-shell/base16-colors.dark.sh"
-[[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
+#BASE16_SHELL="$HOME/.config/base16-shell/base16-default.dark.sh"
+#[[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
 
