8d60065f3ac761ea38f863ea169536f15b149107
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-02-19 22:05:46 +0100

    Cleanup

--- zsh/editor.zsh
+++ zsh/editor.zsh
@@ -1,5 +1,4 @@
 # Only set this if we haven't set $EDITOR up somewhere else previously.
 if [[ "$EDITOR" == "" ]] ; then
-
     if command -v nvim >/dev/null 2>&1; then export EDITOR='nvim'; else export EDITOR='vim'; fi
 fi
