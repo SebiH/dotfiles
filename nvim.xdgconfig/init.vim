ff5e748488269cb44af0acc45c7134d1981819e2
Sebastian Hubenschmid <Sebastian.Hubenschmid@uni-konstanz.de>
2019-08-21 09:36:07 +0200

    WIP

--- nvim.xdgconfig/init.vim
+++ nvim.xdgconfig/init.vim
@@ -1,3 +1,5 @@
+let g:tex_flavor = "latex"
+filetype plugin on
 " Load all config files
 for f in split(glob('~/.config/nvim/conf/*.vim'), '\n')
     exe 'source' f
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,13 +1,13 @@
 alias reload!='. ~/.zshrc'
 
 #Modified commands
-alias ls='ls --color=auto'
+#alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias rsync='rsync --progress'
 
 #New commands
 alias ..='cd ..'
-alias open='xdg-open'
+#alias open='xdg-open'
 
 # Use neovim instead of vim, if possible
 command -v nvim >/dev/null 2>&1 && { alias vim='nvim' }
@@ -23,3 +23,6 @@ if [ $UID -ne 0 ]; then
     # use nivm if installed
     if command -v nvim >/dev/null 2>&1; then alias svim='sudo -E nvim'; else alias svim='sudo -E vim'; fi
 fi
+
+
+export LC_ALL=en_US.UTF-8
